ccfdd1cddc292d0a4a401ba30012069d755b0063
Gentrix miheso <gentrixmiheso90@gmail.com>
2025-09-26 12:18:58 +0300

    Create gentrixMiheso.js

---
+++ gentrixMiheso.js
@@ -0,0 +1,10 @@
+gentrixMiheso.js
+//Define function
+ isEven(num){//check if num is divisible by 2 
+(num%2===0){true;//Even number}else{return false;//odd number}}
+   console.log(is even(4));//true 4 is even 
+   console.log(is even(3));//false 3 is odd
+   console.log(is even(8));//true 8 is even
+   console.log(is even(2));//true 0 is even 
+   console.log(is even(15));//false 15 is odd
+   console.log(is even((7));//false7 is odd
